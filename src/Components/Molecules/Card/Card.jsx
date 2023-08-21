d0c6b0a7151f4a5e557863c2397de873b1a167b8
Nav <navdeep.dhamrait@thebioagency.com>
2023-08-21 16:35:38 -0400

    added cards for projects

--- src/Components/Molecules/Card/Card.jsx
+++ src/Components/Molecules/Card/Card.jsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import '../Card/Card.css';
 
-export default function Card({ name, image, description }) {
+export default function Card({ name, image, description, onClick, sourceCode }) {
     return (
-        <div class='card' id='Projects'>
+        <div onClick={onClick} className='card' id='Projects'>
             <img
                 src={image}
                 alt='Avatar'
                 style={{ width: '100%', height: '200px', objectFit: 'cover' }}
             />
-            <h3 className='card-header'>{name}</h3>
+            <h2 className='card-header'>{name}</h2>
             <p className='card-text-style'>
                 Process of creating and enhancing the user experience of a product or service, with
                 the goal of making it useful, usable, and enjoyable for the users. It involves
                 considering the entire user journey, from the initial interaction with a product to
                 the final goal or task completion.
             </p>
-            <p className='card-text-style'>Tech-Stack - React Js, HTML, CSS</p>
-            <p className='card-text-style'>Source Code- Github</p>
-            <p className='card-text-style'>view site-link</p>
+            <p className='card-text-style'>
+                Tech-<span style={{ color: '#39cad3' }}>Stack</span> - React Js, HTML, CSS
+            </p>
+            <p className='card-text-source-code'>
+                Source <span style={{ color: '#39cad3' }}>code</span>: {sourceCode}
+            </p>
         </div>
     );
 }
--- src/Components/Organisms/ProjectsSection/ProjectsSection.jsx
+++ src/Components/Organisms/ProjectsSection/ProjectsSection.jsx
@@ -4,6 +4,9 @@ import Card from '../../Molecules/Card/Card';
 import ActiveLine from '../../Atoms/ActiveLine/ActiveLine';
 
 const ProjectsSection = () => {
+    const openInNewTab = (url) => {
+        window.open(url, '_blank', 'noreferrer');
+    };
     return (
         <React.Fragment>
             <div className='project-section-container'>
@@ -20,16 +23,22 @@ const ProjectsSection = () => {
                         'https://www.eqbank.ca/images/default-source/default-album/og-image-logo.tmb-opengraph.jpg?Culture=en&sfvrsn=60d787d3_5'
                     }
                     description=''
+                    onClick={() => openInNewTab('https://eqbanksite.netlify.app')}
+                    sourceCode={'https://github.com/nav-commits/eqbanksite'}
                 />
                 <Card
                     name={'Fido'}
                     image={'https://www.fido.ca/app/prod/assets/i/og-image.png'}
                     description=''
+                    onClick={() => openInNewTab('https://fidosite.netlify.app')}
+                    sourceCode={'https://github.com/nav-commits/Fido'}
                 />
                 <Card
                     name={'AfterPay'}
                     image={'https://logowik.com/content/uploads/images/afterpay-new-20215837.jpg'}
                     description=''
+                    onClick={() => openInNewTab('https://afterpaysite.netlify.app')}
+                    sourceCode={'https://github.com/nav-commits/AfterPay'}
                 />
             </div>
         </React.Fragment>
